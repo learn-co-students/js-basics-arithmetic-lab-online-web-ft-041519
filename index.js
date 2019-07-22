bb773b3ebf185976d62d4680d810a091d4c9f009
jpier2012 <jpier2012@gmail.com>
2019-07-22 13:26:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here
+// Write your co
+
+let var1 = 31
+let var2 = 2
+let multiply = var1 * var2
+
+let random = Math.random()
+
+let mod = 16 % 12
+
+let max = Math.max(20,19,18)
